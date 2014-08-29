475044e394b790a01e10f5343b78754f421607cd
Danny Baumann <danthemanvt@gmail.com>
2014-08-29 16:22:03 -0700

    Small test updates

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,36 +2,56 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  let(:base_title) { "Rspecing" }
-
   subject { page }
+
+  shared_examples_for "all static pages" do 
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
 
   describe "Home page" do
     before { visit root_path }
+    let(:heading)    { 'Rspecing' }
+    let(:page_title) { '' }
 
-    it { should have_selector('h1', :text => 'Rspecing') }
-    it { should have_title("#{base_title}") }
+    it_should_behave_like "all static pages"
     it { should_not have_title("Home") }
   end
 
   describe "Help page" do 
     before { visit help_path }
+    let(:heading)    { 'Help' }
+    let(:page_title) { 'Help' }
 
-    it { should have_selector('h1', :text => 'Help') }
-    it { should have_title("#{base_title} | Help") }
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do 
     before { visit about_path }
+    let(:heading)    { 'About' }
+    let(:page_title) { 'About' }
 
-    it { should have_selector('h1', :text => 'About') }
-    it { should have_title("#{base_title} | About") }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
+    let(:heading)    { 'Contact' }
+    let(:page_title) { 'Contact' }
 
-    it { should have_selector('h1', :text => 'Contact') }
-    it { should have_title("#{base_title} | Contact") }
+    it_should_behave_like "all static pages"
+  end
+
+  it "should have the right links on the layout" do 
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
   end
 end
